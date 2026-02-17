359240359a7e2e5b75ce91f5fc9433bb3f6074b2
malli13082008 <malli13082008@gmail.com>
2026-02-17 15:37:07 +0530

    UC3: Refactor banner using String.join() for efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,21 @@
+/**
+ * OOPSBannerApp - UC3: String Join Implementation
+ * @author malli13082008
+ * @version 1.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *     *  *");
-        System.out.println("*     *  *     *  *****    ***** ");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println(" *****    *****    *         ***** ");
-
+        // UC3: Use String.join() for efficient banner line construction
+        String[] line1Parts = {"*", "*", "*", "*", "*", "*", "*", "*"};
+        String[] line2Parts = {"*", "O", "O", "P", "P", "S", "*", "*"};
+        String[] line3Parts = {"*", "*", "*", "*", "*", "*", "*", "*"};
+        
+        String line1 = String.join("", line1Parts);
+        String line2 = String.join("", line2Parts);
+        String line3 = String.join("", line3Parts);
+        
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
     }
 }

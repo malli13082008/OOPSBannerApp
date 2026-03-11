c0e9506c50cc67413c1d21a10f620c9c5ef41840
malli13082008 <malli13082008@gmail.com>
2026-03-11 13:11:22 +0530

    Added UC6 Static Functions Banner App

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,41 @@
-public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-
-        String[] banner = {
-
-            String.join("   ", " *** ", " *** ", " ***** ", " ***** "),
-            String.join("   ", "*   *", "*   *", "*    *", "  *     "),
-            String.join("   ", "*   *", "*   *", "*    *", "  *      "),
-            String.join("   ", "*   *", "*   *", " ***** ", " ***** "),
-            String.join("   ", "*   *", "*   *", "*      ", "      *"),
-            String.join("   ", "*   *", "*   *", "*      ", "      *"),
-            String.join("   ", " *** ", " *** ", "*      ", " ***** ")
-
+ for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + " " +
+                oPattern[i] + " " +public class uc6{
+	  public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
         };
-
-        for (String line : banner) {
-            System.out.println(line);
+    }
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        };
+    }
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+                pPattern[i] + " " +
+                sPattern[i]
+            );
         }
     }
 }
-
